2b8b6ebbc52551a191c946d8c7461e8236453d65
J Cole Morrison <me@jcolemorrison.com>
2024-02-26 19:55:04 -0800

    remove forwarding rule output temp

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-output "forwarding_rule_ip" {
-  description = "The IP address of the global forwarding rule"
-  value       = google_compute_global_forwarding_rule.forwarding_rule.ip_address
-}
+# output "forwarding_rule_ip" {
+#   description = "The IP address of the global forwarding rule"
+#   value       = google_compute_global_forwarding_rule.forwarding_rule.ip_address
+# }

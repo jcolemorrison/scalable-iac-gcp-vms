b5c77697b62fe8eb315f2d9bc2ec46f1f743598a
J Cole Morrison <me@jcolemorrison.com>
2024-02-26 20:28:07 -0800

    formatting

--- compute.tf
+++ compute.tf
@@ -1,7 +1,6 @@
-// Instance template that defines the properties for each instance in the instance group
+# Instance template that defines the properties for each instance in the instance group
 resource "google_compute_instance_template" "server" {
-  count = length(var.deployment_regions)
-
+  count        = length(var.deployment_regions)
   name_prefix  = "instance-template-${var.deployment_regions[count.index]}"
   machine_type = "n1-standard-1"
 
@@ -28,13 +27,12 @@ resource "google_compute_instance_template" "server" {
   }
 }
 
-// Instance group manager that manages instances based on the instance template
+# Instance group manager that manages instances based on the instance template
 resource "google_compute_region_instance_group_manager" "server" {
-  count = length(var.deployment_regions)
-
-  name               = "instance-group-manager-${var.deployment_regions[count.index]}"
-  base_instance_name = "${var.project_name}-instance-${var.deployment_regions[count.index]}"
-  region             = var.deployment_regions[count.index]
+  count               = length(var.deployment_regions)
+  name                = "instance-group-manager-${var.deployment_regions[count.index]}"
+  base_instance_name  = "${var.project_name}-instance-${var.deployment_regions[count.index]}"
+  region              = var.deployment_regions[count.index]
   version {
     instance_template = element(google_compute_instance_template.server.*.self_link, count.index)
   }
@@ -47,28 +45,28 @@ resource "google_compute_region_instance_group_manager" "server" {
   }
 }
 
-// Backend service that routes incoming traffic to the appropriate instance group
+# Backend service that routes incoming traffic to the appropriate instance group
 resource "google_compute_backend_service" "backend" {
-  name        = "global-backend"
-  protocol    = "HTTP"
-  timeout_sec = 10
-  enable_cdn  = false
+  name                 = "global-backend"
+  protocol             = "HTTP"
+  timeout_sec          = 10
+  enable_cdn           = false
   load_balancing_scheme = "EXTERNAL_MANAGED"
 
   dynamic "backend" {
     for_each = google_compute_region_instance_group_manager.server
     content {
-      group = backend.value.instance_group
-      balancing_mode = "UTILIZATION"
-      max_utilization = 0.8
-      capacity_scaler = 1
+      group            = backend.value.instance_group
+      balancing_mode   = "UTILIZATION"
+      max_utilization  = 0.8
+      capacity_scaler  = 1
     }
   }
 
   health_checks = [google_compute_health_check.default.self_link]
 }
 
-// Health check that checks the health of the instances in the instance group
+# Health check that checks the health of the instances in the instance group
 resource "google_compute_health_check" "default" {
   name               = "default"
   check_interval_sec = 1
@@ -78,19 +76,19 @@ resource "google_compute_health_check" "default" {
   }
 }
 
-// URL map that maps URLs to backend services
+# URL map that maps URLs to backend services
 resource "google_compute_url_map" "url_map" {
   name            = "url-map"
   default_service = google_compute_backend_service.backend.self_link
 }
 
-// HTTP proxy that uses the URL map to route incoming requests
+# HTTP proxy that uses the URL map to route incoming requests
 resource "google_compute_target_http_proxy" "proxy" {
   name    = "http-proxy"
   url_map = google_compute_url_map.url_map.self_link
 }
 
-// Global forwarding rule that forwards incoming traffic to the HTTP proxy
+# Global forwarding rule that forwards incoming traffic to the HTTP proxy
 resource "google_compute_global_forwarding_rule" "forwarding_rule" {
   name                = "forwarding-rule"
   target              = google_compute_target_http_proxy.proxy.self_link
